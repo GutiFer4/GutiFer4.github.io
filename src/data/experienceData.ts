3d2aef58b498b9a3baadb79c823567ca26d44e25
GutiFer4 <marcos.gutifer@gmail.com>
2025-06-20 17:43:15 +0200

    Update experienceData.ts

--- src/data/experienceData.ts
+++ src/data/experienceData.ts
@@ -7,29 +7,31 @@ export type Job = {
 
 export const jobs: Job[] = [
   {
-    title: "Data Analyst",
-    company: "Tech Solutions Inc.",
-    dates: "01/2019 - 12/2022",
+    title: "Técnico de Sistemas",
+    company: "VIEWNEXT",
+    dates: "03/2022 - 06/2022",
     description: [
-      "Developed interactive Power BI dashboards to analyze business performance and customer trends.",
-      "Automated ETL processes using Python and SQL to improve data processing efficiency by 40%.",
-      "Collaborated with stakeholders to identify key performance metrics and optimize reporting strategies.",
-      "Designed predictive models to forecast revenue and customer churn, providing actionable insights.",
-      "Managed data extraction from multiple sources, including APIs, databases, and cloud storage.",
-      "Presented analytical findings to executive teams, leading to data-driven decision-making."
+      "Proporcioné soporte Helpdesk, asistiendo a empleados con hardware, software y consultas generales de TI, incluyendo también resolución de problemas en remoto.",
+      "Utilicé sistemas de tickets para rastrear y resolver problemas técnicos de manera eficiente.",
+      "Cableé y configuré un Centro de Datos (CPD) para la instalación de nuevos servidores, colaborando en su puesta en marcha.",
+      "Preparé nuevos espacios de trabajo y salas de reuniones, asegurando que los empleados contaran con todos los recursos necesarios.",
+      "Maqueté y configuré equipos informáticos, adaptando sistemas según las necesidades específicas de los usuarios.",
+      "Gestioné el inventario de TI, garantizando que los empleados dispusieran de las herramientas y materiales necesarios para el correcto desarrollo de su puesto."
+      "Administré el almacenamiento y seguridad de backups, asegurando su correcto almacenamiento y preparación para traslado fuera de sitio."
+      "Supervisé alertas de seguridad y colaboré en la respuesta a incidencias utilizando herramientas de protección y monitorización."
     ]
   },
   {
-    title: "Software Engineer",
-    company: "Innovatech Solutions",
-    dates: "03/2023 - Present",
+    title: "Sistemas y Explotación",
+    company: "RTVE",
+    dates: "04/2024 - 06/2024",
     description: [
-      "Developed full-stack web applications using React, Node.js, and MongoDB.",
-      "Implemented RESTful APIs and integrated third-party services for enhanced application functionality.",
-      "Optimized SQL queries and database schemas to improve system performance.",
-      "Led a team of junior developers, mentoring them on best practices in software engineering.",
-      "Designed and implemented CI/CD pipelines, reducing deployment time by 50%.",
-      "Collaborated with UX/UI designers to enhance the user experience of web applications."
+      "WSL para integración de entornos Linux en Windows.",
+      "Docker para contenerizar aplicaciones y entender su funcionamiento básico.",
+      "Docker Compose, desarrollando una aplicación de prueba para aprender a orquestar aplicaciones multi-contenedor de forma eficiente.",
+      " Configuración de una pila LAMP como parte del aprendizaje en infraestructura.",
+      "Uso de GitLab para control de versiones e integración continua (CI/CD), asegurando el seguimiento y prueba de cada cambio en la aplicación.",
+      "Kubernetes, instalando un clúster OKD completo y desplegando una aplicación similar a la creada con Docker Compose, diseñada para ejecutarse en el entorno del clúster."
     ]
   }
 ];
