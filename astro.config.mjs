cef5cecaddd07a14de9ac99af23dad1f862aaa1f
GutiFer4 <marcos.gutifer@gmail.com>
2025-06-20 18:29:41 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://GutiFer4.github.io',
+  output: 'static',
   integrations: [
     react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
+    tailwind({ applyBaseStyles: false }),
   ],
 });
