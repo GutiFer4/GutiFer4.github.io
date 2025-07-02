089609d6a074be346bdcedb860f2e221f8b45713
GutiFer4 <marcos.gutifer@gmail.com>
2025-07-02 23:06:05 +0200

    Create footerData.ts

---
+++ src/data/footerData.ts
@@ -0,0 +1,15 @@
+export const footerData = {
+  currentYear: new Date().getFullYear(),
+  socialLinks: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/GutiFer4',
+      icon: 'Github'
+    },
+    {
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/marcos-gutierrez-fernandez/',
+      icon: 'Linkedin'
+    }
+  ]
+};
