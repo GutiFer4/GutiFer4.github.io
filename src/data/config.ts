fe063d32aea8e0a5070ea1de0d39027da8c6410a
GutiFer4 <marcos.gutifer@gmail.com>
2025-07-03 18:01:34 +0200

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -2,7 +2,7 @@ export const config = {
   siteTitle: 'Marcos Gutiérrez Portfolio',
   description: 'Soy Marcos Gutiérrez, especializado en Ciberseguridad y Hacking Ético. Formación en SMR, ASIR (especialidad en ciberseguridad) y con un Master finalizado en Ciberseguridad en DIGITECH. Aquí muestro mis proyectos y experiencia.',
   author: 'Marcos Gutiérrez',
-  contactEmail: 'mailto:marcosgutierrez.contacto@gmail.com',
+  contactEmail: 'marcosgutierrez.contacto@gmail.com',
   keywords: 'ciberseguridad, hacking ético, SMR, ASIR, DIGITECH, pentesting, seguridad informática, portfolio',
   favicon: '/favicon.svg',
   social: {
